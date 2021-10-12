1b7aff2ff5a0bba1aca59c7cbcad0994bbe9b1ff
Kenneth Cassel <kennethcassel@gmail.com>
2021-10-12 14:20:40 -0500

    changed copy

---
+++ pages/equipment/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Equipment() {
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+      <Head>
+        <title>Music Equipment for sale</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      Music equipment for sale
+    </div>
+  );
+}
---
+++ pages/list/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Equipment() {
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+      <Head>
+        <title>List your equipment for sale</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      List your equipment for sale
+    </div>
+  );
+}
