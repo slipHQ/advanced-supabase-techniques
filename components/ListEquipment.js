896344df94ffe34fd6311378361ea84d9d35a1b6
Kenneth Cassel <kennethcassel@gmail.com>
2021-10-19 13:56:22 -0500

    fix: small styling stuff

--- components/ListEquipment.js
+++ components/ListEquipment.js
@@ -62,214 +62,216 @@ export default function Account({ session }) {
   }
 
   return (
-    <div className='mx-8'>
-      <div>
-        <div className='md:grid md:grid-cols-3 md:gap-6'>
-          <div className='md:col-span-1'>
-            <div className='px-4 sm:px-0'>
-              <h3 className='text-lg font-medium leading-6 text-gray-900'>
-                Profile
-              </h3>
-              <p className='mt-1 text-sm text-gray-600'>
-                This info will be available on your listing
-              </p>
-            </div>
-          </div>
-          <div className='mt-5 md:mt-0 md:col-span-2'>
-            <div className='shadow sm:rounded-md sm:overflow-hidden'>
-              <div className='px-4 py-5 space-y-6 bg-white sm:p-6'>
-                <div className='grid grid-cols-3 gap-6'>
-                  <div className='col-span-3 sm:col-span-2'>
-                    <label
-                      htmlFor='user-email'
-                      className='block text-sm font-medium text-gray-700'
-                    >
-                      Email
-                    </label>
-                    <div>
-                      <input
-                        className='block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-yellow-300 focus:ring focus:ring-yellow-200 focus:ring-opacity-50'
-                        id='email'
-                        type='email'
-                        value={session.user.email}
-                        disabled
-                      />
-                    </div>
-                  </div>
-                </div>
-
-                <div>
-                  <label
-                    htmlFor='about'
-                    className='block text-sm font-medium text-gray-700'
-                  >
-                    Name
-                  </label>
-                  <div className='mt-1'>
-                    <div>
-                      <input
-                        id='username'
-                        className='block w-1/2 mt-1 border-gray-300 rounded-md shadow-sm focus:border-yellow-300 focus:ring focus:ring-yellow-200 focus:ring-opacity-50'
-                        type='text'
-                        value={username || ""}
-                        onChange={(e) => setUsername(e.target.value)}
-                      />
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className='px-4 py-3 text-right bg-gray-50 sm:px-6'>
-                <button
-                  className='inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-yellow-700 bg-yellow-100 border border-transparent rounded-md hover:bg-yellow-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500'
-                  onClick={() =>
-                    updateProfile({ username, website, avatar_url })
-                  }
-                  disabled={loading}
-                >
-                  {loading ? "Loading ..." : "Update"}
-                </button>
+    <div className='px-4 mx-auto max-w-7xl sm:px-6 lg:px-8'>
+      <div className='max-w-5xl p-6 mx-auto border rounded-lg'>
+        <div>
+          <div className='md:grid md:grid-cols-3 md:gap-6'>
+            <div className='md:col-span-1'>
+              <div className='px-4 sm:px-0'>
+                <h3 className='text-lg font-medium leading-6 text-gray-900'>
+                  Profile
+                </h3>
+                <p className='mt-1 text-sm text-gray-600'>
+                  This info will be available on your listing
+                </p>
               </div>
             </div>
-          </div>
-        </div>
-      </div>
-
-      <div className='hidden sm:block' aria-hidden='true'>
-        <div className='py-5'>
-          <div className='border-t border-gray-200' />
-        </div>
-      </div>
-
-      <div className='mt-10 sm:mt-0'>
-        <div className='md:grid md:grid-cols-3 md:gap-6'>
-          <div className='md:col-span-1'>
-            <div className='px-4 sm:px-0'>
-              <h3 className='text-lg font-medium leading-6 text-gray-900'>
-                Equipment Information
-              </h3>
-              <p className='mt-1 text-sm text-gray-600'>List your gear!</p>
-            </div>
-          </div>
-          <div className='mt-5 md:mt-0 md:col-span-2'>
-            <div className='overflow-hidden shadow sm:rounded-md'>
-              <div className='px-4 py-5 bg-white sm:p-6'>
-                <div className='grid grid-cols-6 gap-6'>
-                  <div className='col-span-6 sm:col-span-3'>
-                    <label
-                      htmlFor='first-name'
-                      className='block text-sm font-medium text-gray-700'
-                    >
-                      Item Name
-                    </label>
-                    <input
-                      type='text'
-                      name='first-name'
-                      id='first-name'
-                      autoComplete='given-name'
-                      placeholder='Keith Moon Drumset'
-                      className='block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-yellow-500 focus:border-yellow-500 sm:text-sm'
-                    />
-                  </div>
-
-                  <div className='col-span-6 sm:col-span-6 lg:col-span-2'>
-                    <label
-                      htmlFor='city'
-                      className='block text-sm font-medium text-gray-700'
-                    >
-                      Amount $
-                    </label>
-                    <input
-                      type='number'
-                      name='city'
-                      id='city'
-                      className='block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-yellow-500 focus:border-yellow-500 sm:text-sm'
-                    />
-                  </div>
-                </div>
-                <div className='my-4'>
-                  <label
-                    htmlFor='about'
-                    className='block text-sm font-medium text-gray-700'
-                  >
-                    Item Description
-                  </label>
-                  <div className='mt-1'>
-                    <textarea
-                      id='about'
-                      name='about'
-                      rows={3}
-                      className='block w-full mt-1 border border-gray-300 rounded-md shadow-sm focus:ring-yellow-500 focus:border-yellow-500 sm:text-sm'
-                      placeholder='This drumset was used by Keith Moon'
-                      defaultValue={""}
-                    />
-                  </div>
-                  <div className='mt-4'>
-                    <label className='block text-sm font-medium text-gray-700'>
-                      Cover photo
-                    </label>
-                    <div className='flex justify-center px-6 pt-5 pb-6 mt-1 border-2 border-gray-300 border-dashed rounded-md'>
-                      <div className='space-y-1 text-center'>
-                        <svg
-                          className='w-12 h-12 mx-auto text-gray-400'
-                          stroke='currentColor'
-                          fill='none'
-                          viewBox='0 0 48 48'
-                          aria-hidden='true'
-                        >
-                          <path
-                            d='M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02'
-                            strokeWidth={2}
-                            strokeLinecap='round'
-                            strokeLinejoin='round'
-                          />
-                        </svg>
-                        <div className='flex text-sm text-gray-600'>
-                          <label
-                            htmlFor='file-upload'
-                            className='relative font-medium text-yellow-600 bg-white rounded-md cursor-pointer hover:text-yellow-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-yellow-500'
-                          >
-                            <span>Upload a file</span>
-                            <input
-                              id='file-upload'
-                              name='file-upload'
-                              type='file'
-                              className='sr-only'
-                            />
-                          </label>
-                          <p className='pl-1'>or drag and drop</p>
-                        </div>
+            <div className='mt-5 md:mt-0 md:col-span-2'>
+              <div className='shadow sm:rounded-md sm:overflow-hidden'>
+                <div className='px-4 py-5 space-y-6 bg-white sm:p-6'>
+                  <div className='grid grid-cols-3 gap-6'>
+                    <div className='col-span-3 sm:col-span-2'>
+                      <label
+                        htmlFor='user-email'
+                        className='block text-sm font-medium text-gray-700'
+                      >
+                        Email
+                      </label>
+                      <div>
+                        <input
+                          className='block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-yellow-300 focus:ring focus:ring-yellow-200 focus:ring-opacity-50'
+                          id='email'
+                          type='email'
+                          value={session.user.email}
+                          disabled
+                        />
                       </div>
                     </div>
-                  </div>{" "}
-                </div>
-              </div>
+                  </div>
 
-              <div></div>
-              <div className='px-4 py-3 text-right bg-gray-50 sm:px-6'>
-                <button
-                  type='submit'
-                  className='inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-yellow-700 bg-yellow-100 border border-transparent rounded-md hover:bg-yellow-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500'
-                >
-                  Publish Listing
-                </button>
+                  <div>
+                    <label
+                      htmlFor='about'
+                      className='block text-sm font-medium text-gray-700'
+                    >
+                      Name
+                    </label>
+                    <div className='mt-1'>
+                      <div>
+                        <input
+                          id='username'
+                          className='block w-1/2 mt-1 border-gray-300 rounded-md shadow-sm focus:border-yellow-300 focus:ring focus:ring-yellow-200 focus:ring-opacity-50'
+                          type='text'
+                          value={username || ""}
+                          onChange={(e) => setUsername(e.target.value)}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className='px-4 py-3 text-right bg-gray-50 sm:px-6'>
+                  <button
+                    className='inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-yellow-700 bg-yellow-100 border border-transparent rounded-md hover:bg-yellow-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500'
+                    onClick={() =>
+                      updateProfile({ username, website, avatar_url })
+                    }
+                    disabled={loading}
+                  >
+                    {loading ? "Loading ..." : "Update"}
+                  </button>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
 
-      <div className='hidden sm:block' aria-hidden='true'>
-        <div className='py-5'>
-          <div className='border-t border-gray-200' />
+        <div className='hidden sm:block' aria-hidden='true'>
+          <div className='py-5'>
+            <div className='border-t border-gray-200' />
+          </div>
         </div>
-      </div>
 
-      <button
-        className='inline-flex items-center justify-end px-3 py-2 text-sm font-medium leading-4 text-yellow-700 bg-yellow-100 border border-transparent rounded-md hover:bg-yellow-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500'
-        onClick={() => supabase.auth.signOut()}
-      >
-        Sign Out
-      </button>
+        <div className='mt-10 sm:mt-0'>
+          <div className='md:grid md:grid-cols-3 md:gap-6'>
+            <div className='md:col-span-1'>
+              <div className='px-4 sm:px-0'>
+                <h3 className='text-lg font-medium leading-6 text-gray-900'>
+                  Equipment Information
+                </h3>
+                <p className='mt-1 text-sm text-gray-600'>List your gear!</p>
+              </div>
+            </div>
+            <div className='mt-5 md:mt-0 md:col-span-2'>
+              <div className='overflow-hidden shadow sm:rounded-md'>
+                <div className='px-4 py-5 bg-white sm:p-6'>
+                  <div className='grid grid-cols-6 gap-6'>
+                    <div className='col-span-6 sm:col-span-3'>
+                      <label
+                        htmlFor='first-name'
+                        className='block text-sm font-medium text-gray-700'
+                      >
+                        Item Name
+                      </label>
+                      <input
+                        type='text'
+                        name='first-name'
+                        id='first-name'
+                        autoComplete='given-name'
+                        placeholder='Keith Moon Drumset'
+                        className='block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-yellow-500 focus:border-yellow-500 sm:text-sm'
+                      />
+                    </div>
+
+                    <div className='col-span-6 sm:col-span-6 lg:col-span-2'>
+                      <label
+                        htmlFor='city'
+                        className='block text-sm font-medium text-gray-700'
+                      >
+                        Amount $
+                      </label>
+                      <input
+                        type='number'
+                        name='city'
+                        id='city'
+                        className='block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-yellow-500 focus:border-yellow-500 sm:text-sm'
+                      />
+                    </div>
+                  </div>
+                  <div className='my-4'>
+                    <label
+                      htmlFor='about'
+                      className='block text-sm font-medium text-gray-700'
+                    >
+                      Item Description
+                    </label>
+                    <div className='mt-1'>
+                      <textarea
+                        id='about'
+                        name='about'
+                        rows={3}
+                        className='block w-full mt-1 border border-gray-300 rounded-md shadow-sm focus:ring-yellow-500 focus:border-yellow-500 sm:text-sm'
+                        placeholder='This drumset was used by Keith Moon'
+                        defaultValue={""}
+                      />
+                    </div>
+                    <div className='mt-4'>
+                      <label className='block text-sm font-medium text-gray-700'>
+                        Cover photo
+                      </label>
+                      <div className='flex justify-center px-6 pt-5 pb-6 mt-1 border-2 border-gray-300 border-dashed rounded-md'>
+                        <div className='space-y-1 text-center'>
+                          <svg
+                            className='w-12 h-12 mx-auto text-gray-400'
+                            stroke='currentColor'
+                            fill='none'
+                            viewBox='0 0 48 48'
+                            aria-hidden='true'
+                          >
+                            <path
+                              d='M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02'
+                              strokeWidth={2}
+                              strokeLinecap='round'
+                              strokeLinejoin='round'
+                            />
+                          </svg>
+                          <div className='flex text-sm text-gray-600'>
+                            <label
+                              htmlFor='file-upload'
+                              className='relative font-medium text-yellow-600 bg-white rounded-md cursor-pointer hover:text-yellow-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-yellow-500'
+                            >
+                              <span>Upload a file</span>
+                              <input
+                                id='file-upload'
+                                name='file-upload'
+                                type='file'
+                                className='sr-only'
+                              />
+                            </label>
+                            <p className='pl-1'>or drag and drop</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>{" "}
+                  </div>
+                </div>
+
+                <div></div>
+                <div className='px-4 py-3 text-right bg-gray-50 sm:px-6'>
+                  <button
+                    type='submit'
+                    className='inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-yellow-700 bg-yellow-100 border border-transparent rounded-md hover:bg-yellow-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500'
+                  >
+                    Publish Listing
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className='hidden sm:block' aria-hidden='true'>
+          <div className='py-5'>
+            <div className='border-t border-gray-200' />
+          </div>
+        </div>
+
+        <button
+          className='inline-flex items-center justify-end px-3 py-2 text-sm font-medium leading-4 text-yellow-700 bg-yellow-100 border border-transparent rounded-md hover:bg-yellow-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500'
+          onClick={() => supabase.auth.signOut()}
+        >
+          Sign Out
+        </button>
+      </div>
     </div>
   );
 }
